0169adb850a4c30791692934ef1ed774d57b403e
datsfilipe <datsfilipe@gmail.com>
2021-10-23 13:04:14 -0300

    add error view render

--- lib/nlw_tags_web/views/error_view.ex
+++ lib/nlw_tags_web/views/error_view.ex
@@ -18,4 +18,15 @@ defmodule NlwTagsWeb.ErrorView do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end
 
+  def render("error.json", %{result: %Changeset{} = changeset}) do
+    %{result: translate_errors(changeset)}
+  end
+
+  defp translate_errors(changeset) do
+    traverse_errors(changeset, fn {msg, opts} ->
+      Enum.reduce(opts, msg, fn {key, value}, acc ->
+        String.replace(acc, "%{#{key}}", to_string(value))
+      end)
+    end)
+  end
 end
